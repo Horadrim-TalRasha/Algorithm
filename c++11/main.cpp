041f6723421a8367084175855bb7d17404deb7cc
spitbreak <524948250@qq.com>
2015-10-06 15:51:07 +0800

    modified

--- c++11/main.cpp
+++ c++11/main.cpp
@@ -6,6 +6,26 @@
 
 class foo
 {
+public:
+    foo() : a(0)
+    {
+        printf("constructor called a = %d\n", a);
+    }
+
+    foo(const foo& s) : a(s.a)
+    {
+//        printf("copy constructor called s.a = %d\n", s.a);
+//        a = s.a;
+    }
+
+    foo& operator=(foo& s)
+    {
+        printf("operator = called s.a = %d a = %d\n", s.a, a);
+        a = s.a;
+        return *this;
+    }
+
+    int a;
 };
 
 
@@ -30,11 +50,17 @@ int main(int argc, char** argv)
 {
     value(1);
     int a = 19;
-    value(a);
+    value(std::move(a));
 
 
     std::string str1("Hello");
     std::string str2 = std::move(str1);
     std::cout << "after move str:" << str1 <<std::endl;    // str1 was cleared
+
+    foo t;
+    t.a = 16;
+    foo s = t; // call copy constructor
+    foo v;
+    v = s;  // call operator==
     return 0;
 }
