63f28b304eb8720eff26ffb5a9b0a147d4264868
spitbreak <524948250@qq.com>
2016-01-31 14:36:14 +0800

    add tutorial of vlan & nis

---
+++ open-sources/nis/tutor/ubuntu server10.4 config NIS_files/ajs.php
@@ -0,0 +1,4 @@
+var OX_e4a50949 = '';
+OX_e4a50949 += "<"+"a href=\'//gg.51cto.com/www/delivery/ck.php?oaparams=2__bannerid=1090__zoneid=460__cb=c8b58a184d__oadest=http%3A%2F%2Fgaozhao.51cto.com%2Fact%2Fcto%2F2%3Fblogchenggong\' target=\'_blank\'><"+"font color=\'red\' style=\"text-decoration:underline\"> CTO训练营第二期强势来袭！期待您的参与\n";
+OX_e4a50949 += "<"+"/font><"+"/a><"+"div id=\'beacon_c8b58a184d\' style=\'position: absolute; left: 0px; top: 0px; visibility: hidden;\'><"+"img src=\'//gg.51cto.com/www/delivery/lg.php?bannerid=1090&amp;campaignid=269&amp;zoneid=460&amp;loc=1&amp;referer=http%3A%2F%2Fxuchengji.blog.51cto.com%2F160472%2F400864&amp;cb=c8b58a184d\' width=\'0\' height=\'0\' alt=\'\' style=\'width: 0px; height: 0px;\' /><"+"/div>\n";
+document.write(OX_e4a50949);
