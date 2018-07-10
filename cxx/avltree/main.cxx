9c3c9af0dd12986619fc7e13df7355a35678d9a5
root <root@lichou.com>
2018-07-10 08:48:24 +0800

    implementing insert API

--- cxx/avltree/main.cxx
+++ cxx/avltree/main.cxx
@@ -5,15 +5,12 @@
 
 class AVLTree
 {
-public:
-    AVLTree();
-    ~AVLTree();
-
 private:
     class AVLTreeNode
     {
     public:
         AVLTreeNode();
+        AVLTreeNode(const int & nodeValue);
         ~AVLTreeNode();
 
     private:
@@ -23,9 +20,16 @@ private:
         AVLTreeNode * m_rightNode;
     };
 
+public:
+    AVLTree();
+    ~AVLTree();
 
+    AVLTreeNode * Insert(const int & nodeValue);
+
+private:
     int __height(const AVLTreeNode * node);
-    int __diff(cont AVLTreeNode * node);
+    int __diff(const AVLTreeNode * node);
+    AVLTreeNode * __searchInsertPointer(const int & nodeValue);
 
     AVLTreeNode * m_rootNode;
 };
@@ -44,8 +48,40 @@ AVLTree::~AVLTree()
 
 }
 
+AVLTree::AVLTreeNode *
+AVLTree::Insert(const int & nodeValue)
+{
+    if(m_rootNode == NULL)
+    {
+        m_rootNode = new AVLTree::AVLTreeNode(nodeValue);
+        return m_rootNode;
+    }
+    else
+    {
+
+   
+    }
+}
+
+AVLTree::AVLTreeNode *
+AVLTree::__searchInsertPointer(const int & nodeValue)
+{
+
+
+
+}
+
 AVLTree::AVLTreeNode::AVLTreeNode() :
 m_nodeValue(0),
+m_leftNode(NULL),
+m_rightNode(NULL)
+{
+
+
+}
+
+AVLTree::AVLTreeNode::AVLTreeNode(const int & nodeValue) :
+m_nodeValue(nodeValue),
 m_leftNode(NULL),
 m_rightNode(NULL)
 {
