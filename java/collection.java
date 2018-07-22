caf8229c88cc1f447efca8c312ee74f20f1f6549
root <root@lichou.com>
2018-07-22 21:51:29 +0800

    add usage of collection class

---
+++ java/collection.java
@@ -0,0 +1,15 @@
+import java.util.Collection;
+import java.util.ArrayList;
+
+
+class collection
+{
+
+
+    public static void main(String[] args)
+    {
+        Collection<String> testArrayList = new ArrayList<String>();
+        testArrayList.add("");
+    }
+}
+
