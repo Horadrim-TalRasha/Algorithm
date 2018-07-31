2cb7aee0e65eab9b3df4c01b15d9ab92b2b648a1
Li, Chao <chao.li@emc.com>
2018-07-30 23:23:48 -1100

    how to get public virtual function address

---
+++ cxx/vtable/main.cxx
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+class A
+{
+public:
+    virtual void func(const int & a) { printf("class A\n"); };
+};
+
+class B : virtual public A
+{
+public:
+    virtual void func(const int & a) { printf("class B\n"); };
+
+};
+
+class C : virtual public A
+{
+public:
+    virtual void func(const int & a) { printf("class C\n"); };
+};
+
+class D : public B, public C
+{
+public:
+    virtual void func(const int & a) { printf("class D\n"); };
+};
+
+class NonInherit
+{
+public:
+    int m_memberA;
+};
+
+int main(int argc, char** argv)
+{
+    NonInherit c;
+	D d;
+    printf("vptr: %p\n", *(int *)&d);
+    printf("first virtual func addr: %p\n", *(int *)*(int *)&d);
+    typedef void(*Func)(const int &);
+	Func fun = (Func)(*(int *)*(int *)(&d));
+	fun(15);
+
+//	Func fun2 = (Func)(*(int *)*(int *)(&d) + 1);
+//	fun2(17);    // segment fault
+    return 0;
+}
