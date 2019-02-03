2ef2ed493b1386eed05e170fbbde96031429267b
ehiclxa <chao.l.li@ericsson.com>
2019-02-03 14:45:55 +0800

    generate dependency not successful

--- code_in_work/ericisson/c/common.mk
+++ code_in_work/ericisson/c/common.mk
@@ -7,19 +7,30 @@ INCLUDE_PATH = -I ../
 
 SRC := $(wildcard ./*.c)
 OBJS := $(SRC:./%.c=./$(BUILD_DIR_NAME)/%.o)
-
-
+DEPS := $(SRC:./%.c=./$(BUILD_DIR_NAME)/%.d)
 
 
 all:
 
 mk_build_dir:
 	@mkdir -p $(BUILD_DIR_NAME)
+	@echo "i shall be first"
 
 check_depends:
 
 
 $(BUILD_DIR_NAME)/%.o: %.c
 	$(CC) $(CFLAGS) $(INCLUDE_PATH) -c $< -o $@
+
+
+$(BUILD_DIR_NAME)/%.d: %.c
+	@echo "WTF"
+	$(CC) -MM $(INCLUDE_PATH) $(CFLAGS) $< > $@
+
+sinclude $(DEPS)
+
+
+clean::
+	rm -rf $(BUILD_DIR_NAME) $(BIN) $(SHARE) $(ARCHIVE)
 
 .PHONY: all mk_build_dir check_depends
